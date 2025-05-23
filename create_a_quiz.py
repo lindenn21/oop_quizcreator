53bbb383e69332bee4a89a93dd3596bbb44266b2
Linden <lindenpowell218@gmail.com>
2025-05-24 00:54:02 +0800

    AddQuestions has been added

--- create_a_quiz.py
+++ create_a_quiz.py
@@ -4,4 +4,15 @@ class CollectedData:
         self.questions = []
         self.file = open(self.file_name, 'w')
 
+    def AddQuestion(self):
+        question = input(f'Enter a question')
+        choice_a = input("Choice A: ")
+        choice_b = input("Choice B: ")
+        choice_c = input("Choice C: ")
+        choice_d = input("Choice D: ")
+        answer = input("What is the correct answer? (A, B, C OR D): ").upper()
+
+
+
+
 

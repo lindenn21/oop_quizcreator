6c88bc08f0946e10475b6d4d15fa50640a5d7e1a
Linden <lindenpowell218@gmail.com>
2025-05-24 08:03:04 +0800

    Added and pasted my timer in a class, testing it as well

---
+++ 2.py
@@ -0,0 +1,12 @@
+import time
+class Timer:
+    def CountDown(self):
+        print("Take a breather, you're about to take the quiz in")
+        time.sleep(1)
+        for i in range(3, 0, -1):
+            print(f"{i}\n")
+            time.sleep(1)
+        print("GOODLUCK!!")
+
+quiz = Timer()
+quiz.CountDown()
